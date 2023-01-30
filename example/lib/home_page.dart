7883cd0e4a4769325d11555f009f9bf8bda33079
jheime <jheime.silveira@condoconta.com.br>
2023-01-30 09:09:00 -0300

    teste de .lock

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -3,7 +3,6 @@ import 'package:example/button_page.dart';
 import 'package:flutter/material.dart';
 
 import 'dropdown_button_page.dart';
-import 'xpto_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -38,14 +37,6 @@ class _HomePageState extends State<HomePage> {
           pageController.jumpToPage(index);
         },
       ),
-      SideMenuItem(
-        priority: 1,
-        title: 'Xpto',
-        onTap: (index, _) {
-          menuController.changePage(index);
-          pageController.jumpToPage(index);
-        },
-      ),
     ];
   }
 
@@ -65,7 +56,6 @@ class _HomePageState extends State<HomePage> {
               children: const [
                 ButtonPage(),
                 DropdownButtonPage(),
-                XptoPage(),
               ],
             ),
           ),
